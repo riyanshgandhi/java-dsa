c56b73f5608a1096a5b87927351d2c2d397539ee
riyanshgandhi <riyanshdlw@gmail.com>
2025-04-22 22:44:18 +0530

    3rd code

---
+++ Reverse an Array.java
@@ -0,0 +1,15 @@
+
+
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+    
+       for (int i=0; i<arr.length/2; i++){
+        //   System.out.println(arr[i]+ " " + arr[arr.length-1-i]);
+        //   arr[arr.length-1-i];
+        int temp = arr[i];
+        arr[i]=arr[arr.length-1-i];
+        arr[arr.length-1-i]=temp;
+       }
+    }
+}
