7c490c7ffa1c9eef2e722a3ee5734db04d6f5312
riyanshgandhi <riyanshdlw@gmail.com>
2025-05-12 23:01:05 +0530

    37th code

---
+++ Search in a row-wise sorted matrix.java
@@ -0,0 +1,21 @@
+
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    private boolean binarySearch(int row[],int x){
+        int left=0, right=row.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(row[mid]==x)return true;
+            else if(row[mid]<x)left=mid+1;
+            else right=mid-1;
+        }
+        return false;
+    }
+    public boolean searchRowMatrix(int[][] mat, int x) {
+       
+       for(int[] row : mat){
+           if(binarySearch(row, x)) return true;
+       }
+        return false;
+    }
+}
